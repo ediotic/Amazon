9c85ea08292d9a56e62628e9bc40e5e25b8f0bfa
Vishal Sharma <vishalsharma@Vishals-Laptop.local>
2024-12-20 13:31:52 +0530

    update auth-screen

--- lib/view/auth/auth_screen.dart
+++ lib/view/auth/auth_screen.dart
@@ -24,132 +24,177 @@ class _AuthScreenState extends State<AuthScreen> {
         automaticallyImplyLeading: true,
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                SizedBox(height: 10,),
-                Text(
-                  "Sign in with your email and password",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                ),
-                 SizedBox(
-                  height: 40,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Sign-in",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Forgot Password?",
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 17,
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text(
+                          "Sign in with your email and password",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w400),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Sign-in",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Forgot Password?",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      hintText: "Email"),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
+                      SizedBox(height: 10),
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          hintText: "Email",
+                        ),
                       ),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      hintText: "Password"),
-                ),
-                CheckboxListTile(
-                  value: true,
-                  onChanged: (value) {},
-                  title: Text("Show Password"),
-                ),
-                CheckboxListTile(
-                  value: true,
-                  onChanged: (value) {},
-                  title: Text("Keep me signed in"),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width / 2 - 40,
-                      vertical: 10),
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 4,
-                        offset: Offset(2, 1),
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          hintText: "Password",
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      CheckboxListTile(
+                        value: true,
+                        onChanged: (value) {},
+                        title: Text("Show Password"),
+                      ),
+                      CheckboxListTile(
+                        value: true,
+                        onChanged: (value) {},
+                        title: Text("Keep me signed in"),
+                      ),
+                      SizedBox(height: 20),
+                      InkWell(
+                        onTap: () {
+                          debugPrint("hello sign in");
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width / 2 - 40,
+                              vertical: 10),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 4,
+                                offset: Offset(2, 1),
+                              ),
+                            ],
+                            color: Colors.amber,
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.black,
+                              endIndent: 10,
+                              thickness: 0.1,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              "New To Amazon",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.black,
+                              indent: 10,
+                              thickness: 0.1,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width / 4 - 30,
+                              vertical: 10),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 4,
+                                offset: Offset(2, 1),
+                              ),
+                            ],
+                            color: Colors.amber[300],
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Create A New Amazon Account",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
                       ),
                     ],
-                    color: Colors.amber,
-                  ),
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
                   ),
                 ),
-                SizedBox(
-                  height: 40,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        color: Colors.black,
-                        endIndent: 10,
-                        thickness: 0.1,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        "New To Amazon",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        color: Colors.black,
-                        indent: 10,
-                        thickness: 0.1,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Create A New Amazon Account",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "Conditions of Use Privacy Notice",
+                style: TextStyle(color: Colors.blue, fontSize: 16),
+              ),
+            ),
+          ],
         ),
       ),
     );
